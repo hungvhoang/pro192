db95b6a9491845c5484b8c1238dd76c2749d35d3
HungHoang <hunghvse183800@fpt.edu.vn>
2023-05-15 09:48:53 +0700

    fix part2

--- workshop1/Part2.java
+++ workshop1/Part2.java
@@ -4,20 +4,7 @@ will print out the result of the specified expression that bases on the inputted
 import java.util.Scanner;
 
 public class Part2 {
-    
-    static int sum(int a, int b){
-        return a+b;
-    }
-    static int sub(int a, int b){
-        return a-b;
-    }
-        static int multiply(int a, int b){
-        return a*b;
-    }
-        static int div(int a, int b){
-        return a/b;
-    }
-
+     
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.print("Input the number 1: ");
@@ -28,16 +15,16 @@ public class Part2 {
         char op = sc.next().charAt(0);
         switch(op){
             case '+':
-                System.out.printf("%d+%d= %d\n",num1,num2,sum(num2, num2));
+                System.out.printf("%d+%d= %d\n",num1,num2, num1 + num2);
                 break;
             case '-':
-                System.out.printf("%d-%d= %d\n",num1,num2,sub(num2, num2));
+                System.out.printf("%d-%d= %d\n",num1,num2,num1 - num2);
                 break;
             case '*':
-                System.out.printf("%d*%d= %d\n",num1,num2,multiply(num2, num2));
+                System.out.printf("%d*%d= %d\n",num1,num2,num1 * num2);
                 break;
             case '/':
-                System.out.printf("%d/%d= %d\n",num1,num2,div(num2, num2));
+                System.out.printf("%d/%d= %d\n",num1,num2,num1 / num2);
                 break;
             default :
                 System.out.println("Not supported.");    
